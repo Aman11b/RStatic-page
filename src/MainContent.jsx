d45cd41fb79d9b649bcf0dd45a62193ced893cca
Aman11b <amansingh1100b@gmail.com>
2025-01-19 20:34:14 +0530

    final commit(was practice project)

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -3,8 +3,9 @@ export default function MainContent(){
     <main>
         <h1>Reasons I am excited to learn React</h1>
         <ol>
-          <li>React feels artistic</li>
-          <li>Its feels like i will be able to develop fast</li>
+          <li>React feels artistic.</li>
+          <li>Its feels like I will be able to develop fast.</li>
+          <li>Its exciting.</li>
         </ol>
       </main>
   )
